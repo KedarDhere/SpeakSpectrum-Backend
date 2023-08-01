88feebe63ad87b17788e219dded25344dcf5988b
KedarDhere <dherek@oregonstate.edu>
2023-07-31 22:02:38 -0700

    Commiting all the files

---
+++ postgresql.js
@@ -0,0 +1,23 @@
+const pg = require('pg')
+
+const pool = new pg.Pool({
+  user: process.env.USERNAME,
+  host: process.env.HOST,
+  database: process.env.DATABASENAME,
+  password: process.env.PASSWORD,
+  port: 5432,
+});
+
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+  } else {
+      console.log('Connected to the database');
+  }
+  done();
+});
+
+// Make the pool available to other parts of the application
+module.exports = { pool };
+
+
